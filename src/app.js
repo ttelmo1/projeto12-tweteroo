89468fab61a802611d51cc136a6b99d2a8070b72
ttelmo1 <telmobsj@gmail.com>
2023-01-06 12:43:15 -0300

    corrigindo req

--- src/app.js
+++ src/app.js
@@ -6,18 +6,20 @@ server.use(express.json());
 
 const server = express();
 const PORT = 5000;
-const USER = { username: req.body.username, avatar: req.body.avatar };
-const TWEET = { username: req.body.username, tweet: req.body.tweet };
+
+
 
 const UserArr = [];
 const TweetArr = [];
 
 server.post('/sign-up', (req, res) => {
+    const USER = { username: req.body.username, avatar: req.body.avatar };
     UserArr.push(USER);
     res.send(USER);
 });
 
 server.post('/tweets', (req, res) => {
+    const TWEET = { username: req.body.username, tweet: req.body.tweet };
     for (let i = 0; i < UserArr.length; i++) {
         if (UserArr[i].username === TWEET.username) {
             TweetArr.push(TWEET);
