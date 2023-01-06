5c9e6281958cb808055c9177ca0ea128d0424383
ttelmo1 <telmobsj@gmail.com>
2023-01-06 11:59:51 -0300

    post sign-up criado

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
 import express from "express";
 import cors from "cors";
 
-const server = express();
-const PORT = 5000;
-
 server.use(cors());
 server.use(express.json());
+
+const server = express();
+const PORT = 5000;
+const USER = { username: req.body.username, avatar: req.body.avatar };
+const TWEET = { username: req.body.username, tweet: req.body.tweet };
+
+const UserArr = [];
+const TweetArr = [];
+
+server.post('/sign-up', (req, res) => {
+    UserArr.push(USER);
+    res.send(USER);
+});
+
+server.post('/tweets', (req, res) => {
+    for(let i = 0; i < UserArr.length; i++) {
+        if(UserArr[i].username === TWEET.username) {
+            TweetArr.push(TWEET);
+            res.send("OK");
+        }
+        else{
+            res.send("UNAUTHORIZED");
+        }
+    }
+});
+
+
+
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-
-server.get("/", (req, res) => {
-    res.send("Hello World");
-}
-);
 
