fda064d2bd0e7e845b7ad975a4a8aa8b8d429a70
ttelmo1 <telmobsj@gmail.com>
2023-01-06 11:58:30 -0300

    post criado

--- src/app.js
+++ src/app.js
@@ -2,17 +2,18 @@ import express from "express";
 import cors from "cors";
 
 const server = express();
+const PORT = 5000;
 
 server.use(cors());
 server.use(express.json());
 
-server.listen(5000, () => {
-    console.log("Server is running on port 5000");
-    });
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 server.get("/", (req, res) => {
     res.send("Hello World");
-    }
-    );
-    
+}
+);
+
